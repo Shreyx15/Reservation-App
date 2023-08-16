3d7ee97603731e7624416f900ac6a188943883d8
Shrey <shreyv15@gmail.com>
2023-08-16 23:04:12 +0530

    did few css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,17 @@
 import Header from "../../components/Header/Header";
 import Navbar from "../../components/Navbar/Navbar";
+import Featured from "../../components/featured/Featured";
 import "./home.css";
 
 function Home() {
     return (
-        <>
+        <div>
             <Navbar />
             <Header />
-        </>
+            <div className="homeContainer">
+                <Featured />
+            </div>
+        </div>
     );
 }
 
