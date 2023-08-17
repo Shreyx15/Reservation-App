750f22df8d1bdd66aa9fa11fd033225c7451bab9
Shrey <shreyv15@gmail.com>
2023-08-17 23:07:26 +0530

    did some css

---
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,11 @@
+import React from './featuredProperties.css ';
+
+const FeaturedProperties = () => {
+    return (
+        <div className='fp'>
+            <img src="" alt="" className="fpImg" />
+        </div>
+    );
+};
+
+export default FeaturedProperties;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Header from "../../components/Header/Header";
 import Navbar from "../../components/Navbar/Navbar";
 import Featured from "../../components/featured/Featured";
+import PropertyList from "../../components/propertyList/propertyList";
 import "./home.css";
 
 function Home() {
@@ -10,6 +11,8 @@ function Home() {
             <Header />
             <div className="homeContainer">
                 <Featured />
+                <h1 className="homeTitle">Browse by Property Type</h1>
+                <PropertyList />
             </div>
         </div>
     );
