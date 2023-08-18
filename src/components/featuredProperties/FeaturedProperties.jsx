5eacd9aec95fade58629e86351fa2a55866addbe
Shrey <shreyv15@gmail.com>
2023-08-18 22:51:26 +0530

    did some css

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -1,9 +1,62 @@
-import React from './featuredProperties.css ';
+import './featuredProperties.css';
 
 const FeaturedProperties = () => {
     return (
         <div className='fp'>
-            <img src="" alt="" className="fpImg" />
+            <div className="fpItem">
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/87428762.webp?k=de5db8fe94cbfe08d3bf16d3c86def035fd73b43ee497cffe27b03363764e0e2&o=" alt="" className="fpImg" />
+                <span className="fpName">3 Epoques Apartments by Prague Residences</span>
+                <span className="fpCity">Amsterdam</span>
+                <span className="fpPrice">₹41,369</span>
+                <div className="fpRating">
+                    <button>8.9</button>
+                    <span>Excellent</span>
+                </div>
+            </div>
+
+            <div className="fpItem">
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/29466558.webp?k=7f9cf4736f69b30c20fa7a751bb8711fa195bc9ff6092d5412d52daf6cada17f&o=" alt="" className="fpImg" />
+                <span className="fpName">3 Epoques Apartments by Prague Residences</span>
+                <span className="fpCity">Amsterdam</span>
+                <span className="fpPrice">₹41,369</span>
+                <div className="fpRating">
+                    <button>8.9</button>
+                    <span>Excellent</span>
+                </div>
+            </div>
+
+            <div className="fpItem">
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/477071028.webp?k=c9d87661dfac89970e50379629aa316a4905033f8f4a13cb21fd28484256d626&o=" alt="" className="fpImg" />
+                <span className="fpName">3 Epoques Apartments by Prague Residences</span>
+                <span className="fpCity">Amsterdam</span>
+                <span className="fpPrice">₹41,369</span>
+                <div className="fpRating">
+                    <button>8.9</button>
+                    <span>Excellent</span>
+                </div>
+            </div>
+
+            <div className="fpItem">
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/64768746.webp?k=0c33d15be1f0c9ebd0ede9b85565c3233ef836884a40d785dd6b36f9f0f50c04&o=" alt="" className="fpImg" />
+                <span className="fpName">3 Epoques Apartments by Prague Residences</span>
+                <span className="fpCity">Amsterdam</span>
+                <span className="fpPrice">₹41,369</span>
+                <div className="fpRating">
+                    <button>8.9</button>
+                    <span>Excellent</span>
+                </div>
+            </div>
+
+            <div className="fpItem">
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/477085213.webp?k=29caadf3e1563bb00c8f93a2af40946a5205edd01343aeb5703ff6740613f747&o=" alt="" className="fpImg" />
+                <span className="fpName">3 Epoques Apartments by Prague Residences</span>
+                <span className="fpCity">Amsterdam</span>
+                <span className="fpPrice">₹41,369</span>
+                <div className="fpRating">
+                    <button>8.9</button>
+                    <span>Excellent</span>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 import Header from "../../components/Header/Header";
 import Navbar from "../../components/Navbar/Navbar";
 import Featured from "../../components/featured/Featured";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
+import MailList from "../../components/mailList/MailList";
 import PropertyList from "../../components/propertyList/propertyList";
 import "./home.css";
 
@@ -13,6 +15,8 @@ function Home() {
                 <Featured />
                 <h1 className="homeTitle">Browse by Property Type</h1>
                 <PropertyList />
+                <FeaturedProperties />
+                <MailList />
             </div>
         </div>
     );
